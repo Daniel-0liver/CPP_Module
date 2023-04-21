f0d1cb722ea1f1def90d322f9914e6e5ad619f31
Daniel-0liver <dtoliverbr@gmail.com>
2023-04-21 01:30:57 +0100

    ex00 done

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -6,25 +6,28 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 15:57:17 by dateixei          #+#    #+#             */
-/*   Updated: 2023/04/20 18:00:08 by dateixei         ###   ########.fr       */
+/*   Updated: 2023/04/21 01:29:01 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstring>
-int main(int argc, char *argv[])
-{
-	
-	if (argc == 1)
+#include <cctype>
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int j = 1; j < argc; j++)
-		{
-			for (size_t i = 0; i < strlen(argv[j]); i++)
-				putwchar(toupper(argv[j][i]));
+		return 0;
+    }
+    for (int i = 1; i < argc; i++) {
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.length(); j++) {
+			str[j] = toupper(str[j]);
 		}
-	}	
-		
-	return (0);
+		std::cout << str;
+		if (argv[i + 1])
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
