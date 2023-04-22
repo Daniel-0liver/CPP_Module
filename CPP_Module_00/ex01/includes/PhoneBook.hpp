c4157b3243266cefdee9a3e0f38e0d880d173db3
Daniel-0liver <dtoliverbr@gmail.com>
2023-04-22 03:15:36 +0100

    Begining of ex01 and files of ex02

---
+++ CPP_Module_00/ex01/includes/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 15:23:56 by dateixei          #+#    #+#             */
+/*   Updated: 2023/04/21 15:40:45 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP_
+# define PHONE_BOOK_HPP_
+# define BLACK	"\033[1;30m"
+# define RED	"\033[1;31m"
+# define GREEN	"\033[1;32m"
+# define YELLOW	"\033[1;33m"
+# define BLUE	"\033[1;34m"
+# define WHITE	"\033[1;37m"
+# define ENDC	"\033[0m"
+
+#include <iostream>
+
+#endif
