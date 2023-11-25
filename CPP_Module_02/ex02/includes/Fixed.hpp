b697857facf347e9bdc56fd685f8af29dc854a5d
Daniel-0liver <dtoliverbr@gmail.com>
2023-11-25 15:31:59 +0000

    CPP02 ex 01 Updated forbidden keyword

--- CPP_Module_02/ex02/includes/Fixed.hpp
+++ CPP_Module_02/ex02/includes/Fixed.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/19 01:30:38 by dateixei          #+#    #+#             */
-/*   Updated: 2023/11/25 15:17:33 by dateixei         ###   ########.fr       */
+/*   Created: 2023/11/02 18:18:26 by dateixei          #+#    #+#             */
+/*   Updated: 2023/11/25 13:11:03 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,17 @@
 #ifndef __FIXED_HPP_
 # define __FIXED_HPP_
 
-#include <iostream>
-#include <cmath>
+# include <iostream>
+# include <cmath>
 
 class Fixed
 {
 private:
 	int					_rawBits;
-	static const int	_fractionalBits = 8;
+	static const int 	_factionalBits = 8;
 public:
 	Fixed();
 	~Fixed();
-	Fixed( const int intValue );
-	Fixed( const float floatValue );
-	Fixed( const Fixed& copyFixed );
-	Fixed &operator=( Fixed const &operatorFixed );
-	int		getRawBits( void );
-	void	setRawBits( int const raw );
-	float 	toFloat( void ) const;
-	int 	toInt( void ) const;
 };
-
-std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
 
 #endif
