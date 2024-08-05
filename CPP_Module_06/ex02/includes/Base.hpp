2f8d5816665c24f57566e8a64264374706519739
Daniel-0liver <dtoliverbr@gmail.com>
2024-08-05 01:36:34 +0100

    Cpp 06 ex02 done

---
+++ CPP_Module_06/ex02/includes/Base.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 01:21:18 by dateixei          #+#    #+#             */
+/*   Updated: 2024/08/05 01:32:20 by dateixei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define BLUE "\033[94m"
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+
+class Base
+{
+public:
+	virtual ~Base();
+};
+
+class A : public Base
+{
+};
+class B : public Base
+{
+};
+class C : public Base
+{
+};
+
+Base *generate(void);
+void identify(Base *p);
+void identify(Base &p);
