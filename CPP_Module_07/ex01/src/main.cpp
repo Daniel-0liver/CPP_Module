6117288b32ca4a75ccb929ad98e39f8f789beb7d
Daniel-0liver <dtoliverbr@gmail.com>
2024-08-15 01:01:53 +0100

    CPP 07 ex02 in progress

--- CPP_Module_07/ex01/src/main.cpp
+++ CPP_Module_07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 12:18:54 by dateixei          #+#    #+#             */
-/*   Updated: 2024/08/07 01:57:17 by dateixei         ###   ########.fr       */
+/*   Updated: 2024/08/14 23:29:40 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 int main()
 {
 	int array[] = {0, 1, 2, 3};
-	int sizeCount = arraySize(array);
-	iter(array, sizeCount, display<int>);
+	int arrayLength = getArrayLength(array);
+	iter(array, arrayLength, display<int>);
 
 	std::cout << "--------------------------------------------" << std::endl;
 
 	std::string tab[3] = {"Hello", "world", "!"};
-	int sizeTab = arraySize(tab);
+	int sizeTab = getArrayLength(tab);
 	iter(tab, sizeTab, display<std::string>);
 
 	std::cout << "--------------------------------------------" << std::endl;
 
 	float floatArray[5] = {42.0f, 26.5f, 34.2f, 90.78f, 3.14f};
-	int floatArraySize = arraySize(floatArray);
+	int floatArraySize = getArrayLength(floatArray);
 	iter(floatArray, floatArraySize, display<float>);
 
 	return 0;
