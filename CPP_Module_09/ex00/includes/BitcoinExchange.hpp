0cb3291aac4d5815b286912ddd10cb5f9e3be36a
Daniel-0liver <dtoliverbr@gmail.com>
2024-09-14 02:58:50 +0100

    Refactor BitcoinExchange fileChecker method and add checkData function

--- CPP_Module_09/ex00/includes/BitcoinExchange.hpp
+++ CPP_Module_09/ex00/includes/BitcoinExchange.hpp
@@ -6,16 +6,16 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 22:07:10 by dateixei          #+#    #+#             */
-/*   Updated: 2024/09/13 21:09:43 by dateixei         ###   ########.fr       */
+/*   Updated: 2024/09/14 02:30:14 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 #include <string>
-#include <map>
 #include <fstream>
-#include <sstream>
+#include <map>
+#include <cstdlib>
 
 class BitcoinExchange
 {
@@ -26,6 +26,45 @@ private:
 	BitcoinExchange &operator=(const BitcoinExchange &copy);
 
 public:
-	static void		fileChecker(char* file);
-	static void 	checkData(std::map<std::string, double>& dataMap, std::ifstream& dataFile);
+	static void fileChecker(char *file);
+	static void validateData(std::map<std::string, double> &dataMap, std::ifstream &dataFile);
+	static void compareValues(std::map<std::string, double> &dataMap, std::ifstream &inFile);
+	static void getInfo(std::string buffer, std::string &date, double &price);
+	static void searchData(std::map<std::string, double> &dataMap, std::string buffer);
+	static std::string parseDate(std::string buffer);
+
+	class EmptyFile : public std::exception
+	{
+	public:
+		EmptyFile();
+		virtual const char *what() const throw();
+	};
+
+	class ValueOutOfRange : public std::exception
+	{
+	public:
+		ValueOutOfRange();
+		virtual const char *what() const throw();
+	};
+
+	class InvalidInput : public std::exception
+	{
+	public:
+		InvalidInput();
+		virtual const char *what() const throw();
+	};
+
+	class InvalidDate : public std::exception
+	{
+	public:
+		InvalidDate();
+		virtual const char *what() const throw();
+	};
+
+	class DataBaseError : public std::exception
+	{
+	public:
+		DataBaseError();
+		virtual const char *what() const throw();
+	};
 };
--- CPP_Module_09/ex00/src/BitcoinExchange.cpp
+++ CPP_Module_09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 22:07:14 by dateixei          #+#    #+#             */
-/*   Updated: 2024/09/13 21:12:00 by dateixei         ###   ########.fr       */
+/*   Updated: 2024/09/14 02:52:41 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,154 @@ void BitcoinExchange::fileChecker(char *file)
 
 	try
 	{
-		checkData(dataMap, dataFile);
+		validateData(dataMap, dataFile);
+		compareValues(dataMap, inFile);
 	}
 	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
+		inFile.close();
+		dataFile.close();
+		return;
 	}
-
-	inFile.close();
-	dataFile.close();
 }
 
-void checkData(std::map<std::string, double> &dataMap, std::ifstream &dataFile)
+void BitcoinExchange::validateData(std::map<std::string, double> &dataMap, std::ifstream &dataFile)
 {
 	std::string buffer;
 	std::string date;
-	double		price;
+	double price;
 
 	if (!std::getline(dataFile, buffer))
+		throw EmptyFile();
 	{
-		
+		try
+		{
+			getInfo(buffer, date, price);
+			dataMap.insert(std::make_pair(date, price));
+		}
+		catch (const std::exception &e)
+		{
+		}
 	}
+	while (std::getline(dataFile, buffer))
+	{
+		getInfo(buffer, date, price);
+		dataMap.insert(std::make_pair(date, price));
+	}
+}
+
+void BitcoinExchange::compareValues(std::map<std::string, double> &dataMap, std::ifstream &inFile)
+{
+	std::string buffer;
+	if (!std::getline(inFile, buffer))
+		throw EmptyFile();
+	if (buffer != "date | value")
+	{	
+		try
+		{
+			searchData(dataMap, buffer);
+		}
+		catch (const std::exception &e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	while (std::getline(inFile, buffer))
+	{
+		try
+		{
+			searchData(dataMap, buffer);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+}
+
+void BitcoinExchange::searchData(std::map<std::string, double> &dataMap, std::string buffer)
+{
+	std::string key = parseDate(buffer);
+	int pos = buffer.rfind(' ');
+	double value = strtod((buffer.substr(pos)).c_str(), NULL);
+	if (value < 0 || value > 1000)
+		throw ValueOutOfRange();
+	std::map<std::string, double>::iterator it = dataMap.find(key);
+	if (it != dataMap.end())
+		std::cout << key << " => " << value << " = " << (value * it->second) << std::endl;
+	else
+	{
+		it = dataMap.lower_bound(key);
+		if (it != dataMap.end())
+			std::cout << it->first << " => " << value << " = " << (value * it->second) << std::endl;
+		else
+		{
+			--it;
+			std::cout << it->first << " => " << value << " = " << (value * it->second) << std::endl;
+		}
+	}
+}
+
+std::string BitcoinExchange::parseDate(std::string buffer)
+{
+	if (buffer.find_first_not_of("0123456789.-| ") != std::string::npos)
+		throw InvalidInput();
+	if (!std::isdigit(buffer[0]) || !std::isdigit(buffer[1]) || !std::isdigit(buffer[2]) || !std::isdigit(buffer[3]) || buffer[4] != '-')
+		throw InvalidInput();
+	int month = atoi((buffer.substr(5, 6)).c_str());
+	if (buffer[7] != '-' || month < 1 || month > 12)
+		throw InvalidDate();
+	int day = atoi((buffer.substr(8, 9)).c_str());
+	if (buffer[10] != ' ' || day < 1 || day > 31)
+		throw InvalidDate();
+	if (buffer[11] != '|' || buffer[12] != ' ')
+		throw InvalidInput();
+	int pos = buffer.find(' ');
+	return (buffer.substr(0, pos));
+}
+
+void BitcoinExchange::getInfo(std::string buffer, std::string &date, double &price)
+{
+	if (buffer.find_first_not_of("0123456789,-. ") != std::string::npos){
+		throw DataBaseError();
+	}
+	int pos = buffer.find(",");
+	date = buffer.substr(0, pos);
+	price = strtod((buffer.substr(pos + 1)).c_str(), NULL);
+}
+
+BitcoinExchange::EmptyFile::EmptyFile() {}
+
+const char *BitcoinExchange::EmptyFile::what() const throw()
+{
+	return ("Error: File is empty");
+}
+
+BitcoinExchange::ValueOutOfRange::ValueOutOfRange() {}
+
+const char *BitcoinExchange::ValueOutOfRange::what() const throw()
+{
+	return ("Error: this value is not in the range of 0 to 1000");
+}
+
+BitcoinExchange::InvalidInput::InvalidInput() {}
+
+const char *BitcoinExchange::InvalidInput::what() const throw()
+{
+	return ("Error: invalid input or format error");
+}
+
+BitcoinExchange::InvalidDate::InvalidDate() {}
+
+const char *BitcoinExchange::InvalidDate::what() const throw()
+{
+	return ("Error: invalid date or format error");
+}
+
+BitcoinExchange::DataBaseError::DataBaseError() {}
+
+const char *BitcoinExchange::DataBaseError::what() const throw()
+{
+	return ("Error: btc database has invalid data");
 }
